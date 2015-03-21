b83c10174e7d6edea84fa3a690bc80c1e3cb246a
jsrudani <jsrudani@gmail.com>
2015-03-21 13:05:05 -0500

    Initial Commit Compiled on 64 bit Ubuntu 14.04 LTS

---
+++ hadoop-client/target/dist-copynativelibs.sh
@@ -0,0 +1,27 @@
+BUILD_DIR="/home/jsrudani/hadoopcache/hadoopsrccode/hadoop-client/target"
+                      TAR='tar cf -'
+                      UNTAR='tar xfBp -'
+                      LIB_DIR="${BUILD_DIR}/native/target/usr/local/lib"
+                      if [ -d ${LIB_DIR} ] ; then
+                        TARGET_DIR="${BUILD_DIR}/hadoop-client-2.3.0/lib/native"
+                        mkdir -p ${TARGET_DIR}
+                        cd ${LIB_DIR}
+                        $TAR lib* | (cd ${TARGET_DIR}/; $UNTAR)
+                        if [ "false" = "true" ] ; then
+                          cd "${snappy.lib}"
+                          $TAR *snappy* | (cd ${TARGET_DIR}/; $UNTAR)
+                        fi
+                      fi
+                      BIN_DIR="${BUILD_DIR}/bin"
+                      if [ -d ${BIN_DIR} ] ; then
+                        TARGET_BIN_DIR="${BUILD_DIR}/hadoop-client-2.3.0/bin"
+                        mkdir -p ${TARGET_BIN_DIR}
+                        cd ${BIN_DIR}
+                        $TAR * | (cd ${TARGET_BIN_DIR}/; $UNTAR)
+                        if [ "false" = "true" ] ; then
+                          if [ "false" = "true" ] ; then
+                            cd "${snappy.lib}"
+                            $TAR *snappy* | (cd ${TARGET_BIN_DIR}/; $UNTAR)
+                          fi
+                        fi
+                      fi
---
+++ hadoop-client/target/dist-maketar.sh
@@ -0,0 +1,2 @@
+cd "/home/jsrudani/hadoopcache/hadoopsrccode/hadoop-client/target"
+                      tar cf - hadoop-client-2.3.0 | gzip > hadoop-client-2.3.0.tar.gz
